05eb4213dc0a8d69c348e40ae3bdfcd500826f2a
amrmt1270 <akif.3839@keio.jp>
2025-07-29 10:45:18 +0900

    chapter12 completed

--- snsproject/post/urls.py
+++ snsproject/post/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('<int:num>', views.index, name = 'index'),
-    path('post/create/', views.create_post, name = 'create')
+    path('post/create/', views.create_post, name = 'create'),
+    path('post/<int:pk>', views.post_detail, name = 'detail'),
+    path('post/<int:pk>/delete', views.post_delete, name = 'delete'),
+    path('post/<int:pk>/favorite', views.post_favorite, name = 'favorite')
 ]
--- snsproject/post/views.py
+++ snsproject/post/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views import generic
 from .forms import PostForm
-from .models import Post
+from .models import Post, Favorite
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core.paginator import Paginator
@@ -35,3 +35,39 @@ def index(request, num = 1):
         'object_list':data
     }
     return render(request, 'post/home.html', params)
+
+def post_detail(request,pk):
+    data = Post.objects.get(pk = pk)
+    params = {
+        'object' : data 
+    }
+    return render(request, 'post/detail.html', params)
+
+@login_required
+def post_delete(request, pk):
+    if request.method == 'POST':
+        data = Post.objects.get(pk = pk, owner = request.user)
+        data.delete()
+        messages.success(request, 'メッセージを削除しました')
+    else:
+        messages.error(request, '正しい方法で削除してください')
+    return redirect(to = '/')
+
+@login_required
+def post_favorite(request, pk):
+    if request.method == 'POST':
+        data = Post.objects.get(pk = pk)
+        is_favorite = Favorite.objects.filter(owner = request.user).filter(content_id = data).count()
+        if is_favorite > 0:
+            messages.error(request, '既にそのメッセージにはお気に入り登録しています')
+            return redirect(to = '/')
+        data.favorite_count += 1
+        data.save()
+        favorite = Favorite()
+        favorite.owner = request.user
+        favorite.content_id = data
+        favorite.save()
+        messages.success(request, 'メッセージをお気に入り登録しました')
+    else:
+        messages.error(request, '正しい方法でお気に入り登録をしてください')
+    return redirect(to = '/')
